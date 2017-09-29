89f2b22a70175895a0dc14486df35f7f517d6175
kamyshev <kamyshev@localhost.localdomain>
2017-09-29 22:42:37 +0300

    add script

--- docker-1/start.sh
+++ docker-1/start.sh
@@ -2,4 +2,4 @@ docker build -t mynginx -f nginx.Dockerfile .
 docker build -t mytomcat -f tomcat.Dockerfile .
 docker build -t mywebapp -f webapp.Dockerfile .
 docker run --rm -d --name mywebapp mywebapp
-docker run --rm -d --name mynginx --link mywebapp:mywebapp -p 80:80 nginx #
+docker run --rm -d --name mynginx --link mywebapp:mywebapp -p 80:80 mynginx #
